0d824882337392e2d444d57bfcf3bde6aa1007ab
Daniel Bergey <daniel.bergey@braze.com>
2022-07-12 16:37:28 -0400

    task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let text, setText = useState("")
+  const [tasks, setTasks] = useState<string[]>([""])
+
+  const editTask = (key: number) => (event: React.ChangeEvent<HTMLInputElement>) => setTasks((oldTasks: string[]) => {
+    let ret = [...oldTasks]
+    ret[key] = event.target.value;
+    return ret
+  })
+
+  const appendEmpty = () => setTasks(oldTasks => [...oldTasks, ""])
+
+  const deleteTask = (key: number) => () =>
+    setTasks(oldTasks => [...oldTasks.slice(0, key), ...oldTasks.slice(key+1, oldTasks.length)])
+
   return (
     <div className="App">
         <h1>
           Things to do:
         </h1>
         <ul>
-            <li><input type="text" value={text}></input></li>
+            {tasks && tasks.map( (t: string, i: number) => (
+              <li>
+                  <input type="text" key={i} value={t} onChange={editTask(i)}></input>
+                  <button onClick={deleteTask(i)}>-</button>
+              </li>
+))}
         </ul>
+        <button onClick={appendEmpty}>New Task</button>
+
     </div>
   );
 }
