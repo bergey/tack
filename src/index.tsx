5013c53a9d67fc335d6458aa0e175871dc6f1b2b
Daniel Bergey <daniel.bergey@braze.com>
2022-08-20 18:19:40 -0400

    log only in dev build

--- src/index.tsx
+++ src/index.tsx
@@ -45,12 +45,14 @@ async function registerServiceWorker() {
           type: "module",
         }
       );
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
+      if (process.env.NODE_ENV !== "production") {
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
       }
     } catch (error) {
       console.error(`Registration failed with ${error}`);
