ba5d8b86efe4e942e34dbd292a22ec06ac13e8b5
Daniel Bergey <daniel.bergey@braze.com>
2022-10-24 05:39:44 -0400

    WS echo, import Automerge

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,29 @@
 use axum::{
+    extract::ws::{WebSocketUpgrade, WebSocket},
     routing::get,
+    response::Response,
     Router,
 };
+
+async fn ws_upgrade(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(sync_crdt_ws)
+}
+
+async fn sync_crdt_ws(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        if socket.send(msg).await.is_err() {
+            // client disconnected
+            return;
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/ws", get(ws_upgrade));
 
     // run it with hyper on localhost:3003
     axum::Server::bind(&"0.0.0.0:3003".parse().unwrap())
