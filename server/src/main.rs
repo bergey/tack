e0f242a67d212b41c35e7b6391d5f405e45a1f6e
Daniel Bergey <daniel.bergey@braze.com>
2022-10-25 21:21:56 -0400

    receive changes on server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,4 @@
-use automerge::Automerge;
+use automerge::{Automerge, sync};
 use async_mutex::Mutex;
 use axum::{
     extract::ws::{WebSocketUpgrade, WebSocket, self},
@@ -22,20 +22,22 @@ async fn ws_upgrade(ws: WebSocketUpgrade) -> Response {
 }
 
 async fn sync_crdt_ws(mut socket: WebSocket) {
-    while let Some(Ok(msg)) = socket.recv().await {
-        debug!("got WS message: {:?}", msg);
-        match msg {
-            ws::Message::Text(_) =>
-                if socket.send(msg).await.is_err() {
+    let mut sync_state = sync::State::new();
+    while let Some(Ok(ws_msg)) = socket.recv().await {
+        match &ws_msg {
+            ws::Message::Text(txt) => {
+                debug!("got WS message: {}", txt);
+                if socket.send(ws_msg).await.is_err() {
                     // client disconnected
                     return;
-                },
-            ws::Message::Binary(_blob) =>
-                // TODO AM here
-                if socket.send(ws::Message::Text("blob".to_string())).await.is_err() {
-                    // client disconnected
-                    return;
-                },
+                }},
+            ws::Message::Binary(blob) => {
+                // TODO handle errors
+                debug!("got binary WS message");
+                let sync_msg = sync::Message::decode(&blob).unwrap();
+                let mut project = PROJECT.lock().await;
+                project.receive_sync_message(&mut sync_state, sync_msg).unwrap();
+            },
             _ => {}
         }
     }
